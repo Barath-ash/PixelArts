5c6f48160c7b61e9aa21c07a543188d6a4587e80
Barath-ash <bbarath601@gmail.com>
2025-03-07 21:56:20 +0530

    Outline made

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    < >
+    <div className="flex justify-evenly gap-60 p-3 m-0.5 ">
+        <div><h1 className='relative right-12'>PixelArts</h1></div>
+        <div>
+            <ul className='flex justify-evenly gap-10'> 
+              <li>Home</li>
+            <li>About us</li>
+            <li>Contact</li>
+            <li>Cart</li></ul>
+           
+        </div>
+        <div>
+          <p>hey</p>
+        </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default Navbar
