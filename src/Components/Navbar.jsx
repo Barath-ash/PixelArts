5c6cc389a9d1670731a66070c06c8dbedd10769e
Barath-ash <bbarath601@gmail.com>
2025-03-10 19:26:19 +0530

    Navbar completed

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,25 +1,55 @@
 import React from 'react'
+import { FaHome } from "react-icons/fa";
+import { IoIosAlert } from "react-icons/io";
+import { RiContactsBook3Fill } from "react-icons/ri";
+import { FaShoppingCart } from "react-icons/fa";
+import { FaCircleUser } from "react-icons/fa6";
 
 const Navbar = () => {
   return (
-    < >
-    <div className="flex justify-evenly gap-60 p-3 m-0.5 ">
-        <div><h1 className='relative right-12'>PixelArts</h1></div>
+    <> <div className='bg-white shadow-xl'>
+      <div className="flex justify-evenly gap-55 p-4 m-1.5 ">
         <div>
-            <ul className='flex justify-evenly gap-10'> 
-              <li>Home</li>
-            <li>About us</li>
-            <li>Contact</li>
-            <li>Cart</li></ul>
+          <h1 className="relative right-12 text-2xl font-bold">PixelArts</h1>
+        </div>
+        <div>
+          <ul className="flex justify-evenly gap-25 font-serif text-sm/9 font-medium   relative mt-1">
+            <li className="flex font-sm ">
+              <span className="text-black absolute -inset-x-5.5 top-2.5  text-base">
+                <FaHome />
+              </span>
+              HOME
+            </li>
+            <li className='relative'>
+              <span className="text-black absolute -inset-x-5.5 top-2.5 text-base">
+                <IoIosAlert />
+              </span>
+              ABOUT US
+            </li>
+            <li className='relative'>
+            <span className="text-black absolute -inset-x-5.5 top-2.5   text-base">
+            <RiContactsBook3Fill />
+              </span>
+              CONTACT
+            </li>
+
+            <li className='relative'> 
+            <span className="text-black absolute -inset-x-5.5 top-2.5 text-base">
+            <FaShoppingCart />
+              </span>
            
+              CART</li>
+          </ul>
         </div>
-        <div>
-          <p>hey</p>
+        <div className='text-2xl justify-center mt-1.5'>
+           <a href="#"> 
+          <span className=''><FaCircleUser /></span>
+         </a>
         </div>
-    </div>
-      
+      </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Navbar
